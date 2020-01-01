a965ddf806f4214b7e8d933faf6f10ca23796a8c
LiuChangshuo <3170104593@zju.edu.cn>
2020-01-02 03:25:23 +0800

    del

--- js/article.js
+++
@@ -1,7 +0,0 @@
-$(function (){
-  $(".btn-editArticle").click(function(){
-    $("#articleContainer").css("display","none");
-    $("#textareaContainer").css("display","block");
-    $(".btn-editArticle").css("display","none");
-  })
-});
--- php/global.php
+++
@@ -1,7 +0,0 @@
-<?php
-function trimall($str)//删除空格
-{
-    $qian=array(" ","　","\t","\n","\r","\r\n","\\r\\n");$hou=array("","","","","","","");
-    return str_replace($qian,$hou,$str);
-}
-?>
